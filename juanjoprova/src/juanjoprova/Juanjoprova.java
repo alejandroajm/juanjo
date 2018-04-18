6ec968e4ee2fb3413ecab045fa4a86b3bdfb1c24
pc <pc@pc-VirtualBox>
2018-04-18 16:39:23 +0200

    Version 1.0 hola juanjo

--- juanjoprova/src/juanjoprova/Juanjoprova.java
+++ juanjoprova/src/juanjoprova/Juanjoprova.java
@@ -17,6 +17,7 @@ public class Juanjoprova {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("hola juanjo");
+        System.out.print("com estas");
     }
     
 }
